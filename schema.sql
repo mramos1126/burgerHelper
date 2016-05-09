a85a2d54c62f0a13b1c8fab1aaa455d94f295262
Mauricio <Ramos@Mauricios-MBP.home>
2016-05-09 05:47:07 -0400

    homework

---
+++ schema.sql
@@ -0,0 +1,10 @@
+create xb4wyoqwm1u3q942;
+use xb4wyoqwm1u3q942;
+
+CREATE TABLE `Burgers` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `burger_name` VARCHAR(50) ,
+  `devoured` Boolean,
+  `date` TIMESTAMP  ,
+  PRIMARY KEY (`id`)
+);
